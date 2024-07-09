13d72888951b408d78d84b11b88ee946ac3da2c2
Bokyung Kwon <tree445560@gmail.com>
2024-07-09 10:19:17 +0900

    Baekjoon: add
    Baekjoon: 1991 신규 정답 추가

---
+++ Baekjoon_Algorithm_Study/1991_트리,재귀.py
@@ -0,0 +1,36 @@
+# https://www.acmicpc.net/problem/1991
+
+import sys
+
+def preorder(node):
+    print(node, end="")
+    if tree[node][0] != ".":
+        preorder(tree[node][0])
+    if tree[node][1] != ".":
+        preorder(tree[node][1])
+
+def inorder(node):
+    if tree[node][0] != ".":
+        inorder(tree[node][0])
+    print(node, end="")
+    if tree[node][1] != ".":
+        inorder(tree[node][1])
+
+def postorder(node):
+    if tree[node][0] != ".":
+        postorder(tree[node][0])
+    if tree[node][1] != ".":
+        postorder(tree[node][1])
+    print(node, end="")
+    
+N = int(sys.stdin.readline())
+tree = {}
+for _ in range(N):
+    data, left, right = sys.stdin.readline().split()
+    tree[data] = (left, right)
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
