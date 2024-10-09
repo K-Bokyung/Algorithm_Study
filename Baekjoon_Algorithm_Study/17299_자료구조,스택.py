7cb0eb799f3be3ca236e6071c2727acc326333d4
Bokyung Kwon <tree445560@gmail.com>
2024-10-09 11:28:32 +0900

    Baekjoon: edit
    Baekjoon: 17299 풀고 있는 과정 추가

--- Baekjoon_Algorithm_Study/17299_자료구조,스택.py
+++ Baekjoon_Algorithm_Study/17299_자료구조,스택.py
@@ -7,25 +7,24 @@ from collections import deque
 N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
 
-# 2. 수열의 각 원소가 수열 내에서 몇 개인지 확인
-s_A = set(A)
-d_A = deque(A)
-A_F = [d_A.count(i) for i in s_A]
-
-# print(A_F)
-# [[1, 3], [2, 2], [3, 1], [4, 1]]
-
-# 3. 수열을 하나씩 돌면서 오른쪽에 있으면서 등장한 횟수가 큰 수 중 작은 수 찾기
+# 2. 수열을 하나씩 돌면서 오른쪽에 있으면서 등장한 횟수가 큰 수 중 작은 수 찾기
 result = [-1] * N
+stack = [0] * N
 idx = [0]
 
+# for i in range(1, N):
+#   while stack and A.count(stack[-1]) < A.count(A[i]):
+#     result[stack.pop()] = A[i]
+#   stack.append(i)
+
+for i in range(N):
+  stack[A[i]-1] += 1
+
 for i in range(1, N):
-  # print('idx', idx)
-  # print('A[i]', A[i])
-  # print('A_F[A[i]-1]', A_F[A[i]-1])
-  while idx and A_F[A[idx[-1]]-1] < A_F[A[i]-1]:
-    result[idx.pop()] = A_F[A[i]-1]
-    # print('result', result)
+  while idx and stack[idx[-1]] < stack[A[i]-1]:
+    result[idx.pop()] = A[i]
+  result.append(-1)
   idx.append(i)
-  
-print('result', result)
+
+print(stack)
+print(*result)
