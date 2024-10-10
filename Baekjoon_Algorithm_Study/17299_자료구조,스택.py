e5dd9b2b67d100f10fb049facf9fe81b4b7749fd
Bokyung Kwon <tree445560@gmail.com>
2024-10-10 11:41:13 +0900

    Baekjoon: edit
    Baekjoon: 17299 신규 정답으로 수정

--- Baekjoon_Algorithm_Study/17299_자료구조,스택.py
+++ Baekjoon_Algorithm_Study/17299_자료구조,스택.py
@@ -2,29 +2,30 @@
 
 # 1. 입력값 받기
 import sys
-from collections import deque
+from collections import defaultdict
 
 N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
 
 # 2. 수열을 하나씩 돌면서 오른쪽에 있으면서 등장한 횟수가 큰 수 중 작은 수 찾기
+A_cnt = defaultdict(int)
+
+for i in A:
+  if i in A_cnt:
+    A_cnt[i] += 1
+  else:
+    A_cnt[i] = 1
+
 result = [-1] * N
-stack = [0] * N
 idx = [0]
 
-# for i in range(1, N):
-#   while stack and A.count(stack[-1]) < A.count(A[i]):
-#     result[stack.pop()] = A[i]
-#   stack.append(i)
-
-for i in range(N):
-  stack[A[i]-1] += 1
-
 for i in range(1, N):
-  while idx and stack[idx[-1]] < stack[A[i]-1]:
-    result[idx.pop()] = A[i]
-  result.append(-1)
+  while idx and A_cnt[A[idx[-1]]] < A_cnt[A[i]]:
+    result[idx[-1]] = A[i]
+    idx.pop()
   idx.append(i)
 
-print(stack)
 print(*result)
+
+# 리스트로는 시간초과가 나고 딕셔너리로 하니 시간초과가 나지 않음
+# O(n^2)과 O(n)의 차이
