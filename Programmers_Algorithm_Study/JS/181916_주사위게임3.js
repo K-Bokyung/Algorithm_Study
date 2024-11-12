74e561a0bbd7c893ce6e1d1b048dc5b27d347e3f
Bokyung Kwon <tree445560@gmail.com>
2024-11-12 10:12:49 +0900

    Edit: Programmers coding study (JS)
    Programmers: 181916 신규 정답으로 수정(JS)

--- Programmers_Algorithm_Study/JS/181916_주사위게임3.js
+++ Programmers_Algorithm_Study/JS/181916_주사위게임3.js
@@ -5,37 +5,38 @@ function solution(a, b, c, d) {
   let num_list = [a, b, c, d].sort();
   let num_set = new Set(num_list);
 
+  // 1. 모두 같은 숫자
   if (num_set.size === 1) {
     answer = 1111 * a;
-  } else if (num_set.size === 2 && num_list[1] === num_list[2]) {
-    let p = 0;
-    let q = 0;
-    num_list.lastIndexOf(num_list[0]) === 0 ? (p = a) : (q = a);
-    q !== a ? (q = b) : (p = a);
-    answer = (10 * p + q) ** 2;
-  } else if (num_set.size === 2 && num_list[1] !== num_list[2]) {
+  }
+  // 2. 모두 다른 숫자
+  else if (num_set.size === 4) {
+    answer = num_list[0];
+  }
+  // 3. 두 개씩 같은 값
+  else if (num_set.size === 2 && num_list[1] !== num_list[2]) {
     let p = num_list[0];
     let q = num_list[2];
-    let num = (p + q) * (p - q);
-    num < 0 ? (answer = -num) : (answer = num);
-  } else if (num_set.size === 3) {
+    (p + q) * (p - q) < 0 ? (answer = -((p + q) * (p - q))) : (answer = (p + q) * (p - q));
+  }
+  // 4. 두 개만 같은 값
+  else if (num_set.size === 3) {
     let p = num_list[0];
     let q = num_list[2];
     let r = num_list[3];
-    q === num_list[1] ? ((p = num_list[1]), (q = num_list[0])) : (q = num_list[2]);
-    r === num_list[2]
-      ? ((p = num_list[2]), (q = num_list[0]), (r = num_list[3]))
-      : (r = num_list[3]);
+    q === num_list[1] ? ((p = num_list[1]), (q = num_list[0])) : undefined;
+    r === num_list[2] ? ((p = num_list[2]), (q = num_list[0]), (r = num_list[1])) : undefined;
     answer = q * r;
-  } else if (num_set.size === 4) {
-    answer = num_list[0];
+  }
+  // 5. 세 개만 같은 값
+  else if (num_set.size === 2 && num_list[0] !== num_list[3]) {
+    let p = 0;
+    let q = 0;
+    num_list.lastIndexOf(num_list[0]) !== 0
+      ? ((p = num_list[0]), (q = num_list[3]))
+      : ((p = num_list[3]), (q = num_list[0]));
+    answer = (10 * p + q) ** 2;
   }
 
   return answer;
 }
-
-const a = 2;
-const b = 5;
-const c = 2;
-const d = 6;
-console.log(solution(a, b, c, d));
