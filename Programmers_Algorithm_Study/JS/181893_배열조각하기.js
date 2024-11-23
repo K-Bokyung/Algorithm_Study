3215b60747c7b8c214050ff2fdfcc070f4f4a2f5
Bokyung Kwon <tree445560@gmail.com>
2024-11-23 10:31:44 +0900

    Add: Programmers coding study (JS)
    Programmers: 181894, 181895 신규 정답 추가(JS)

---
+++ Programmers_Algorithm_Study/JS/181893_배열조각하기.js
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181893
+
+function solution(arr, query) {
+  let answer = [];
+  return answer;
+}
+
+const n = 4;
+const arr = [1, 2, 1, 2, 1, 10, 2, 1];
+// const intervals = [
+//   [1, 3],
+//   [0, 4],
+// ];
+
+console.log(solution(arr));
