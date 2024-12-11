e34f83d0779648a97be6f5fdaebebe1574244652
Bokyung Kwon <tree445560@gmail.com>
2024-12-11 12:06:15 +0900

    Edit: Programmers coding study (JS)
    Programmers: 181878 풀이 과정 수정(JS)

--- Programmers_Algorithm_Study/JS/181878.원하는문자열찾기.js
+++ Programmers_Algorithm_Study/JS/181878.원하는문자열찾기.js
@@ -2,19 +2,22 @@
 
 function solution(myString, pat) {
   let answer = 0;
-  let low_string = myString.toLowerCase();
-  let low_pat = pat.toLowerCase();
-  let first_s = low_pat[0];
+  let low_string = myString.toLowerCase().split('');
+  let low_pat = pat.toLowerCase().split('');
 
-  if (myString.length < pat.length && !low_string.split('').includes(first_s)) {
-    return answer;
+  if (myString.length < pat.length && !low_string.includes(low_pat[0])) {
+    answer = 0;
   }
 
-  console.log('if문 넘어옴');
+  if (
+    low_string.slice(low_string.indexOf(low_pat[0]), low_pat.length).join('') === low_pat.join('')
+  ) {
+    answer = 1;
+  }
 
   return answer;
 }
 
-const myString = 'aaAA';
-const pat = 'aa';
+const myString = 'AbCdEfG';
+const pat = 'aBc';
 console.log(solution(myString, pat));
