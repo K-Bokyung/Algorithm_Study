8ac46354594d4612a4706a8ff75365ee4422a5da
Bokyung Kwon <tree445560@gmail.com>
2024-12-13 10:54:21 +0900

    Edit: Programmers coding study (JS)
    Programmers: 181878을 정답 코드로 수정(JS)

--- Programmers_Algorithm_Study/JS/181878.원하는문자열찾기.js
+++ Programmers_Algorithm_Study/JS/181878.원하는문자열찾기.js
@@ -2,22 +2,12 @@
 
 function solution(myString, pat) {
   let answer = 0;
-  let low_string = myString.toLowerCase().split('');
-  let low_pat = pat.toLowerCase().split('');
 
-  if (myString.length < pat.length && !low_string.includes(low_pat[0])) {
-    answer = 0;
-  }
-
-  if (
-    low_string.slice(low_string.indexOf(low_pat[0]), low_pat.length).join('') === low_pat.join('')
-  ) {
-    answer = 1;
-  }
+  myString.toLowerCase().includes(pat.toLowerCase()) ? (answer = 1) : (answer = 0);
 
   return answer;
 }
 
 const myString = 'AbCdEfG';
-const pat = 'aBc';
+const pat = 'AbC';
 console.log(solution(myString, pat));
